9212b946a1471beb0af91f2d7f3b2fa952bc2751
Dipesh Kumar Karn <dipeshkumar.karn@nimbus.com>
2024-02-23 19:25:05 +0545

    any message

---
+++ operator.py
@@ -0,0 +1,59 @@
+''' 
+# Arithemetic operators:
+# Addition
+# Operator +
+a = 2+2+2
+print(a)
+b = 1+2+3.6
+print(b)
+word = "dipesh "+"karn"
+print(word)
+'''
+'''
+word_no = "suda"+1
+print(word_no)
+# word_no ="sudan"+1 can only concatenate str (not "int") to str
+'''
+'''
+x = 3-1
+print(a)
+y = 4-1
+print(y)
+e = 3*3
+print(e)
+f = 8/2
+print(f)
+print = e+f
+print(print)
+r = e*f
+print(r)
+number = "2"+"1"
+print(number)
+# sub
+a = 2-2-15-48
+print(a)
+a = 1*3
+print(a)
+word
+'''
+# Division 
+# Operator/
+a = 12/2
+print(a)
+b = 13/.3
+print(b)
+a = 12%5
+print(a)
+a = 4**2
+print(a) # Exaple 4*4
+a = 2+2-2*2/2%2**2
+print(a)
+
+
+
+
+
+
+
+
+
