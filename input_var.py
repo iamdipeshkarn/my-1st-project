70c0b617c295521f3c015b216203636d6c5173bf
Dipesh Kumar Karn <dipeshkumar.karn@nimbus.com>
2024-02-23 19:21:18 +0545

    python

---
+++ input_var.py
@@ -0,0 +1,35 @@
+''''
+a = int(input("enter a value"))
+b = int(input("enter a second"))
+print(a+b)
+print("the addition of " ,a,"and" ,b, "is", a+b)
+
+result = f'the addition of {a} and {b} is {a+b}'
+print(result)
+
+a = int(input("enter a value"))
+b = int(input("enter a value"))
+result = f'the addition of {a} and {b} is {a+b}'
+print(result)
+a = int(input("enter a value"))
+b = int(input("enter a second"))
+
+result1 = f'the addition of {a} and {b} is {a-b}'
+
+result2 = f'the addition of {a} and {b} is {a*b}'
+
+
+print(result1)
+print(result2)
+a = float(input("enter a value"))
+b = float(input("enter second value"))
+print(result 2)
+b = addition 
+'''
+
+
+
+
+
+
+
